6e3ecad004e2cde27b06ed8814b885253a74db0f
filippovivan <94ivan.filippov@gmail.com>
2014-06-16 02:54:10 +0200

    Second try

---
+++ src/by/bsu/filippov/train/launcher/Launcher.java
@@ -0,0 +1,13 @@
+package by.bsu.filippov.train.launcher;
+
+public class Launcher {
+	static {
+		//TODO COnfig logger
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
