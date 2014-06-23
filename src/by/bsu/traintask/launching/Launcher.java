0a0deb1ea6c482d732c1f463193cdd10aea5323e
filippovivan <94ivan.filippov@gmail.com>
2014-06-23 11:42:53 +0200

    Factory added.

--- src/by/bsu/traintask/launching/Launcher.java
+++ src/by/bsu/traintask/launching/Launcher.java
@@ -6,23 +6,22 @@ import org.apache.log4j.xml.DOMConfigurator;
 
 import by.bsu.traintask.enteties.Train;
 import by.bsu.traintask.exceptions.TechnicalException;
-import by.bsu.traintask.builders.TrainBuilderFromJSON;
+import by.bsu.traintask.factories.JSONTrainFactory;
 
 public class Launcher {
 	private static final String TRAIN_JSON_PATH = "resourses/jsontrain.json";
+	private static final Logger LOG = Logger.getLogger(Launcher.class);
 
 	static {
 		new DOMConfigurator().doConfigure("resourses/logconfig.xml",
 				LogManager.getLoggerRepository());
 	}
 
-	private static final Logger LOG = Logger.getLogger(Launcher.class);
-
 	public static void main(String[] args) {
-		TrainBuilderFromJSON parcer = new TrainBuilderFromJSON(TRAIN_JSON_PATH);
+		JSONTrainFactory parcer = new JSONTrainFactory(TRAIN_JSON_PATH);
 		try {
 			LOG.debug("Parsing began");
-			Train train = parcer.buildTrain();
+			Train train = parcer.createInstance();
 			LOG.info(train);
 		} catch (TechnicalException e) {
 			LOG.error(e);
